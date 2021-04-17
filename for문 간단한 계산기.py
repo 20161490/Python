e2a1f7dab8e6d8af27eb95b4fbe3457de82d8fda
강권모 <62058791+20161490@users.noreply.github.com>
2021-04-17 21:54:51 +0900

    Add files via upload

---
+++ for문 간단한 계산기.py
@@ -0,0 +1,30 @@
+print("1.더하기 2.빼기 3.곱하기 4.나누기 5.나가기")
+while True:
+    item=int(input("원하는 기능을 선택하고 번호를 입력하세요"))
+    if item>5:       
+        print("잘못입력하셧습니다.")
+        continue
+    elif item<1:       
+        print("잘못입력하셧습니다.")
+        continue
+    elif item==5:
+        print("종료합니다.")
+        break
+    a=int(input("1번째 숫자"))
+    b=int(input("2번째 숫자"))
+    c=[a+b]
+    d=[a-b]
+    e=[a*b]
+    f=[a/b]
+    for c1 in c:
+        if item==1:
+            print("더하기 결과 : ",c1)
+    for d1 in d:
+        if item==2:
+            print("빼기 결과 : ",d1)
+    for e1 in e:
+        if item==3:
+            print("곱하기 결과 : ",e1)
+    for f1 in f:
+        if item==4:
+            print("나누기 결과 : ",f1)
